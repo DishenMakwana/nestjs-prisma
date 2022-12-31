e8884d34f5e41ced3298b03ffe2bc9ec4e933a51
DISHEN <dishenmakwana.dm@gmail.com>
2022-12-31 15:39:50 +0530

    Fixed: error response return in json

--- src/common/interceptors/error-exception.interceptor.ts
+++ src/common/interceptors/error-exception.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpException,
   InternalServerErrorException,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
@@ -13,35 +14,41 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class ErrorExceptionInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const logger: Logger = new Logger('Error Exception Interceptor');
+
     return next.handle().pipe(
       catchError((error) => {
-        if (error.status)
-          return throwError(
-            () =>
-              new HttpException(
-                {
-                  statusCode: error.status,
-                  success: false,
-                  message: error.message,
-                  stack: error.stack,
-                  error: error,
-                },
-                error.status,
-              ),
-          );
+        if (error.status) {
+          return throwError(() => {
+            logger.error(error.message, error.stack);
 
-        return throwError(
-          () =>
-            new InternalServerErrorException(
+            return new HttpException(
               {
-                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                statusCode: error.status,
+                success: false,
                 message: error.message,
                 stack: error.stack,
                 error: error,
               },
               error.status,
-            ),
-        );
+            );
+          });
+        }
+
+        return throwError(() => {
+          logger.error(error.message, error.stack);
+
+          return new InternalServerErrorException(
+            {
+              statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+              success: false,
+              message: error.message,
+              stack: error.stack,
+              error: error,
+            },
+            error.status,
+          );
+        });
       }),
     );
   }
--- src/common/interceptors/response.interceptor.ts
+++ src/common/interceptors/response.interceptor.ts
@@ -5,6 +5,8 @@ import {
   ExecutionContext,
   HttpException,
   InternalServerErrorException,
+  Logger,
+  HttpStatus,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable, throwError } from 'rxjs';
@@ -19,51 +21,62 @@ export interface Response<T> {
 
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  constructor(private reflector: Reflector) {}
+  constructor(private readonly reflector: Reflector) {}
 
   intercept(
     context: ExecutionContext,
     next: CallHandler,
   ): Observable<Response<T>> {
+    const logger: Logger = new Logger('Response Interceptor');
+
     return next.handle().pipe(
       map((data) => {
         const success_message = this.reflector.get<string[]>(
           'success_message',
           context.getHandler(),
         );
+
         data = camelize(data);
+
         return {
           success: true,
           message: success_message ?? message.SUCCESS_RESPONSE,
           data: data,
         };
       }),
-      catchError((error) => {
-        if (error.status)
-          return throwError(
-            () =>
-              new HttpException(
-                {
-                  success: false,
-                  message: error.message,
-                  stack: error.stack,
-                  error: error,
-                },
-                error.status,
-              ),
-          );
 
-        return throwError(
-          () =>
-            new InternalServerErrorException(
+      catchError((error) => {
+        if (error.status) {
+          return throwError(() => {
+            logger.error(error.message, error.stack);
+
+            return new HttpException(
               {
+                statusCode: error.status,
                 success: false,
                 message: error.message,
                 stack: error.stack,
+                error: error,
               },
               error.status,
-            ),
-        );
+            );
+          });
+        }
+
+        return throwError(() => {
+          logger.error(error.message, error.stack);
+
+          return new InternalServerErrorException(
+            {
+              statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+              success: false,
+              message: error.message,
+              stack: error.stack,
+              error: error,
+            },
+            error.status,
+          );
+        });
       }),
     );
   }
